d87435ef87d4ea54d6d64f4e14cb2925ac758fca
melZula <zula0747@gmail.com>
2021-03-28 19:13:36 +0300

    addition of favorites released

--- 1/script.js
+++ 1/script.js
@@ -5,9 +5,6 @@ document.querySelector('.fv form').addEventListener('submit', addFavorite)
 function getWeather(latitude, longitude) {
     let mainTemplate = document.querySelector('template.main')
     let mainCity = document.importNode(mainTemplate.content, true)
-    let propsTemplate = document.querySelector('template.props')
-    let propsNode = document.importNode(propsTemplate.content, true)
-    let propsList = propsNode.querySelectorAll('li span:last-child')
     let url = new URL('http://api.openweathermap.org/data/2.5/weather')
     params = {
         'lat': latitude,
@@ -29,14 +26,11 @@ function getWeather(latitude, longitude) {
             mainCity.querySelector('h2').textContent = r.name
             mainCity.querySelector('span').textContent = r.main.temp + '°'
             mainCity.querySelector('img').src = 'http://openweathermap.org/img/wn/' + r.weather[0].icon + '@4x.png'
-            propsList[0].textContent = r.wind.speed + ' m/s'
-            propsList[1].textContent = r.weather[0].description
-            propsList[2].textContent = r.main.pressure + 'hpa'
-            propsList[3].textContent = r.main.humidity + '%'
-            propsList[4].textContent = '[' + r.coord.lon + ', ' + r.coord.lat + ']'
+            let propsNode = setPropsList(r)
+            return propsNode
         }
     ).then(
-        () => {
+        (propsNode) => {
             document.getElementById('main').appendChild(mainCity)
             document.getElementById('props-main').appendChild(propsNode)
             loaders.map((l) => { l.style.display = 'none' })
@@ -46,6 +40,18 @@ function getWeather(latitude, longitude) {
     )
 }
 
+function setPropsList(data) {
+    let propsTemplate = document.querySelector('template.props')
+    let propsNode = document.importNode(propsTemplate.content, true)
+    let propsList = propsNode.querySelectorAll('li span:last-child')
+    propsList[0].textContent = data.wind.speed + ' m/s'
+    propsList[1].textContent = data.weather[0].description
+    propsList[2].textContent = data.main.pressure + 'hpa'
+    propsList[3].textContent = data.main.humidity + '%'
+    propsList[4].textContent = '[' + data.coord.lon + ', ' + data.coord.lat + ']'
+    return propsNode
+}
+
 function getLocation() {
     let options = {
         maximumAge: 60000,
@@ -53,10 +59,10 @@ function getLocation() {
         enableHighAccuracy: false
     };
     loaders = Array.from(document.getElementsByClassName("loader"))
+    loaders.map((l) => { l.style.display = 'flex' })
     dataElements = Array.from(document.getElementsByClassName("data"))
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition((pos) => {
-            loaders.map((l) => { l.style.display = 'flex' })
             getWeather(pos.coords.latitude, pos.coords.longitude)
         },
             (err) => { getWeather(55.7522200, 37.6155600) },
@@ -65,8 +71,6 @@ function getLocation() {
         console.log("Geolocation is not supported by this browser.");
     }
 }
-getLocation()
-
 
 function addFavorite(event) {
     let url = new URL('http://api.openweathermap.org/data/2.5/weather')
@@ -87,9 +91,46 @@ function addFavorite(event) {
             return Promise.reject(response.status)
         }
     }).then((r) => {
-        localStorage.getItem("fv") ?? localStorage.setItem('fv', '{[]}')
-        localStorage.setItem('fv', event.target.elements[0].value)
+        localStorage.getItem('fv') ?? localStorage.setItem('fv', '')
+        fvs = localStorage.getItem('fv')
+        localStorage.setItem('fv', fvs + ' ' + event.target.elements[0].value)
+        getFavoriteWeather(event.target.elements[0].value)
     }).catch((e) => {
         alert(e)
     })
+}
+
+function getFavoriteWeather(sityName) {
+    let fvTemplate = document.querySelector('template.fv')
+    let fvCard = document.importNode(fvTemplate.content, true)
+    let url = new URL('http://api.openweathermap.org/data/2.5/weather')
+    params = {
+        'q': sityName,
+        'appid': APIkey,
+        'units': 'metric'
+    }
+    Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
+    fetch(url).then((response) => {
+        if (response.ok) {
+            return response.json()
+        } else {
+            return Promise.reject(response.status)
+        }
+    }).then((r) => {
+        fvCard.querySelector('h3').textContent = sityName
+        fvCard.querySelector('.fvheader span').textContent = r.main.temp + '°'
+        let propsNode = setPropsList(r)
+        return propsNode
+    }).then((propsNode) => {
+        fvCard.querySelector('.favorite').appendChild(propsNode)
+        document.getElementsByClassName('favorites')[0].appendChild(fvCard)
+    })
+}
+
+getLocation()
+let fvList = localStorage.getItem('fv')
+if (fvList !== null && fvList !== undefined) {
+    fvList.split(' ').forEach((sityName) => {
+        getFavoriteWeather(sityName)
+    });
 }
